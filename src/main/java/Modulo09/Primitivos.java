a0e9b3e398af8f75fdfb05bcc540c4e525fa0d0c
Hz2217 <fhanzava@hotmail.com>
2021-10-25 17:36:50 -0300

    Initial commit

---
+++ src/main/java/Modulo09/Primitivos.java
@@ -0,0 +1,17 @@
+package Modulo09;
+
+import java.util.Scanner;
+
+public class Primitivos {
+
+    public static void main(String[] args) {
+        Integer valor ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe um valor: ");
+        valor = input.nextInt();
+        System.out.println(valor);
+        int valor2 = valor;
+        System.out.println(valor2);
+        }
+}
+
