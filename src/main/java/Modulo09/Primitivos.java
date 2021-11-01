b41dbb4ca649b904b0edbe0b75de7828da70c1ac
Hz2217 <fhanzava@hotmail.com>
2021-11-01 13:20:23 -0300

    Updated 2

--- src/main/java/Modulo09/Primitivos.java
+++ src/main/java/Modulo09/Primitivos.java
@@ -6,12 +6,20 @@ public class Primitivos {
 
     public static void main(String[] args) {
         Integer valor ;
+        int valor_primitivo;
         Scanner input = new Scanner(System.in);
         System.out.println("Informe um valor: ");
         valor = input.nextInt();
         System.out.println(valor);
+        System.out.println("Informe um valor: ");
+        valor_primitivo = input.nextInt();
+        System.out.println(valor_primitivo);
+
+        Integer valor_wrapper;
+        valor_wrapper = valor_primitivo;
         int valor2 = valor.intValue();
         System.out.println(valor2);
+        System.out.println(valor_wrapper);
         }
 }
 
